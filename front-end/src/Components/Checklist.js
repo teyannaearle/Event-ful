f392389a6285e80a8c21943d518a92cf531a0092
Teyanna Earle <teyonna.earle92@gmail.com>
2021-09-02 02:40:16 -0400

    Work on budget, checklist, countdown components

---
+++ front-end/src/Components/Checklist.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Checklist({categories}) {
+
+  const listItem = (category) => {
+    let item = "";
+    switch (category) {
+      case "catering":
+        item = "Find a Caterer";
+        break;
+      case "djs":
+        item = "Find a DJ";
+        break;
+      case "musicians":
+        item = "Find a Musician";
+        break;
+      case "partyequipmentrentals":
+        item = "Find Eqipment Rentals";
+        break;
+      case "eventphotography":
+        item = "Find a Photographer";
+        break;
+      case "videographers":
+        item = "Find a Videographer";
+        break;
+      case "venues":
+        item = "Find a Venue";
+        break;
+      case "balloonservices":
+        item = "Find Balloon Services";
+        break;
+      case "floraldesigners":
+        item = "Find a Floral Designer";
+        break;
+      default:
+        item = "";
+    }
+
+    return item;
+  };
+
+  return (
+    <ul>
+      {categories.map((category, i) => {
+        return (
+          <li key={i}>
+            <Link to={category}>{listItem(category)}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default Checklist;
---
+++ front-end/src/Components/Timer.js
@@ -0,0 +1,106 @@
+import React, { useEffect, useState } from "react";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+
+const minuteSeconds = 60;
+const hourSeconds = 3600;
+const daySeconds = 86400;
+
+const timerProps = {
+  isPlaying: true,
+  size: 120,
+  strokeWidth: 6,
+};
+
+const renderTime = (dimension, time) => {
+  return (
+    <div className="time-wrapper">
+      <div className="time">{time}</div>
+      <div>{dimension}</div>
+    </div>
+  );
+};
+
+const getTimeSeconds = (time) => (minuteSeconds - time) | 0;
+const getTimeMinutes = (time) => ((time % hourSeconds) / minuteSeconds) | 0;
+const getTimeHours = (time) => ((time % daySeconds) / hourSeconds) | 0;
+const getTimeDays = (time) => (time / daySeconds) | 0;
+
+function Timer() {
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [updated, setUpdated] = useState(false);
+
+  useEffect(() => {
+    //  date and time  will eventually come from an api call to /events/:user_id/:event_id
+    setDate("2021-09-30");
+    setTime("00:30:00");
+    setUpdated(true);
+  }, []);
+
+  const countDown = () => {
+    const finish = new Date(`${date}T${time}`);
+    const startTime = Date.now() / 1000; 
+    const endTime = finish.getTime() / 1000; 
+    const remainingTime = Math.ceil(endTime - startTime);
+    const days = Math.ceil(remainingTime / daySeconds);
+    const daysDuration = days * daySeconds;
+
+    return (
+      <div>
+        <CountdownCircleTimer
+          {...timerProps}
+          colors={[["#191970"]]}
+          duration={daysDuration}
+          initialRemainingTime={remainingTime}
+        >
+          {({ elapsedTime }) =>
+            renderTime("days", getTimeDays(daysDuration - elapsedTime))
+          }
+        </CountdownCircleTimer>
+        <CountdownCircleTimer
+          {...timerProps}
+          colors={[["#0047AB"]]}
+          duration={daySeconds}
+          initialRemainingTime={remainingTime % daySeconds}
+          onComplete={(totalElapsedTime) => [
+            remainingTime - totalElapsedTime > hourSeconds,
+          ]}
+        >
+          {({ elapsedTime }) =>
+            renderTime("hours", getTimeHours(daySeconds - elapsedTime))
+          }
+        </CountdownCircleTimer>
+        <CountdownCircleTimer
+          {...timerProps}
+          colors={[["#007FFF"]]}
+          duration={hourSeconds}
+          initialRemainingTime={remainingTime % hourSeconds}
+          onComplete={(totalElapsedTime) => [
+            remainingTime - totalElapsedTime > minuteSeconds,
+          ]}
+        >
+          {({ elapsedTime }) =>
+            renderTime("minutes", getTimeMinutes(hourSeconds - elapsedTime))
+          }
+        </CountdownCircleTimer>
+        <CountdownCircleTimer
+          {...timerProps}
+          colors={[["#00B7EB"]]}
+          duration={minuteSeconds}
+          initialRemainingTime={remainingTime % minuteSeconds}
+          onComplete={(totalElapsedTime) => [
+            remainingTime - totalElapsedTime > 0,
+          ]}
+        >
+          {({ elapsedTime }) =>
+            renderTime("seconds", getTimeSeconds(elapsedTime))
+          }
+        </CountdownCircleTimer>
+      </div>
+    );
+  };
+
+  return <div>{updated ? countDown() : null}</div>;
+}
+
+export default Timer;
