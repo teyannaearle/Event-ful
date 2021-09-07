5e3581e7e78f7bcc9c7e86bfc7e26d4e991edd5c
Teyanna Earle <teyonna.earle92@gmail.com>
2021-09-06 20:14:14 -0400

    correct foreign key references and begin adding seeds

--- back-end/db/prod_schema.sql
+++ back-end/db/prod_schema.sql
@@ -2,7 +2,7 @@ DROP DATABASE IF EXISTS partyplanning_dev;
 CREATE DATABASE partyplanning_dev;
 \c partyplanning_dev;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY, 
     username VARCHAR (50) UNIQUE NOT NULL, 
@@ -12,7 +12,7 @@ CREATE TABLE users (
     last_login TIMESTAMP 
 );
 
-DROP TABLE IF EXISTS events;
+DROP TABLE IF EXISTS events CASCADE;
 CREATE TABLE events (
     event_id SERIAL PRIMARY KEY,
     event_name VARCHAR (255) NOT NULL, 
@@ -24,7 +24,7 @@ CREATE TABLE events (
 );
 
 
-DROP TABLE IF EXISTS tasklist;
+DROP TABLE IF EXISTS tasklist CASCADE;
 CREATE TABLE tasklist (
     -- user_id FOREIGN KEY REFERENCES users(user_id)(ON UPDATE CASCADE ON DELETE CASCADE), 
     -- event_id FOREIGN KEY REFERENCES events(event_id)(ON UPDATE CASCADE ON DELETE CASCADE),
@@ -34,16 +34,17 @@ CREATE TABLE tasklist (
     is_completed BOOLEAN NOT NULL DEFAULT FALSE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     user_id  SERIAL, CONSTRAINT fk_tasklist_users FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE,
-    event_id SERIAL, CONSTRAINT fk_tasklist_events FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE
+    event_id SERIAL, CONSTRAINT fk_tasklist_events FOREIGN KEY(event_id) REFERENCES events(event_id) ON DELETE CASCADE
 );
 
-DROP TABLE IF EXISTS booked;
+DROP TABLE IF EXISTS booked CASCADE;
 CREATE TABLE booked (
     -- user_id FOREIGN KEY REFERENCES users(user_id)(ON UPDATE CASCADE ON DELETE CASCADE),
     -- event_id FOREIGN KEY REFERENCES events(event_id)(ON UPDATE CASCADE ON DELETE CASCADE),
     -- task_id FOREIGN KEY REFERENCES tasklist(task_id),
     user_id  SERIAL, CONSTRAINT fk_booked_users FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE,
-    event_id SERIAL, CONSTRAINT fk_booked_events FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    event_id SERIAL, CONSTRAINT fk_booked_events FOREIGN KEY(event_id) REFERENCES events(event_id) ON DELETE CASCADE,
+    task_id SERIAL, CONSTRAINT fk_booked_tasklist FOREIGN KEY(task_id) REFERENCES tasklist(task_id) ON DELETE CASCADE,
     vendor_name VARCHAR (255) NOT NULL, 
     vendor_address VARCHAR (255) NOT NULL,
     vendor_phone_number INT,
@@ -51,7 +52,7 @@ CREATE TABLE booked (
 );
 
     
-DROP TABLE IF EXISTS favorites;
+DROP TABLE IF EXISTS favorites CASCADE;
 CREATE TABLE favorites (
     -- user_id FOREIGN KEY REFERENCES users(user_id)(ON UPDATE CASCADE ON DELETE CASCADE), 
     user_id  SERIAL, CONSTRAINT fk_favorites_users FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE,
--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -1,8 +1,33 @@
-INSERT INTO test (name) VALUES
-('Monday'),
-('Tuesday'),
-('Wednesday'),
-('Thursday'),
-('Friday'),
-('Saturday'),
-('Sunday');
+\c partyplanning_dev;
+
+INSERT INTO users ( username, password, email, created_on, last_login) VALUES
+('tearle92' , 'fakepassword' ,'fakeemail@email.com','2004-10-19 10:23:54', '2004-10-19 10:23:54');
+
+INSERT INTO events ( event_name , event_zip, event_budget, event_date, event_time, user_id ) VALUES
+('lindas baby shower' , 11375, 1000, '2021-09-30', '00:30:00', 1);
+
+INSERT INTO tasklist (task_name , is_completed , user_id, event_id ) VALUES 
+('catering', false, 1, 1) ,
+('djs', false, 1, 1),
+('musicians', false, 1, 1),
+('partyequipmentrentals', false, 1, 1),
+('eventphotography', false, 1, 1),
+('videographers', false, 1, 1),
+('venues', false, 1, 1),
+('balloonservices', false, 1, 1),
+('floraldesigners', false, 1, 1);
+
+-- INSERT INTO booked () VALUES
+-- ()
+
+-- INSERT INTO favorites () VALUES
+-- ()
+
+
+
+
+
+
+
+
+
