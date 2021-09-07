86bb846ee8e808d33465918ccd1ad077f3b64001
Angelina Ebreo <angelinaebreo@gmail.com>
2021-09-07 10:31:20 -0400

    Booked Vendors component

--- back-end/Controllers/eventController.js
+++ back-end/Controllers/eventController.js
@@ -2,20 +2,3 @@ const express = require("express");
 const events = express.Router({ mergeParams: true });
 
 module.exports = events
-
-
-
-// const express = require("express");
-// const events = express.Router({ mergeParams: true });
-
-// const {
-//     getAllEvents,
-//     getEvent,
-//     createEvent,
-//     deleteEvent,
-//     updateEvent,
-//   } = require("../queries/events.js");
-
-  
-  
-//   module.exports = events
