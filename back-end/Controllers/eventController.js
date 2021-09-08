8cddcaf96d3914bf29788bea52548ffd8098d832
Teyanna Earle <teyonna.earle92@gmail.com>
2021-09-08 16:05:40 -0400

    Add backend/eventController.js functions

--- back-end/Controllers/eventController.js
+++ back-end/Controllers/eventController.js
@@ -1,4 +1,120 @@
 const express = require("express");
 const events = express.Router({ mergeParams: true });
+// ----- STILL NEED TO DO REQ.BODY VALIDATION  ----
+const {
+  getAllEvents,
+  getEvent,
+  createEvent,
+  deleteEvent,
+  updateEvent,
+} = require("../queries/events");
 
-module.exports = events
+const db = require("../db/dbConfig");
+
+events.get("/:user_id/:event_id", async (req, res) => {
+  const { user_id, event_id } = req.params;
+
+  try {
+    const event = await getEvent(user_id, event_id);
+
+    if (event.user_id) {
+      res.status(200).json({
+        success: true,
+        payload: event,
+      });
+    } else {
+      throw event;
+    }
+  } catch (e) {
+    res.status(404).json({
+      success: false,
+      message: e,
+    });
+  }
+});
+
+events.get("/:user_id", async (req, res) => {
+  const { user_id } = req.params;
+
+  try {
+    const events = await getAllEvents(user_id);
+
+    if (events[0].user_id) {
+      res.status(200).json({
+        success: true,
+        message: events,
+      });
+    } else {
+      throw events;
+    }
+  } catch (e) {
+    res.status(404).json({
+      success: false,
+      message: e,
+    });
+  }
+});
+
+events.post("/:user_id", async (req, res) => {
+  const { user_id } = req.params;
+
+  try {
+    const newEvent = await createEvent(req.body, user_id);
+
+    if (newEvent.user_id) {
+      res.status(200).json({
+        success: true,
+        payload: newEvent,
+      });
+    } else {
+      throw newEvent;
+    }
+  } catch (e) {
+    res.status(404).json({
+      success: false,
+      message: e,
+    });
+  }
+});
+
+events.delete("/:user_id/:event_id", async (req, res) => {
+  const { user_id, event_id } = req.params;
+
+  try {
+    const deleted = await deleteEvent(user_id, event_id);
+    if (deleted.user_id) {
+      res.status(200).json({ success: true, payload: deleted });
+    } else {
+      throw deleted;
+    }
+  } catch (e) {
+    res.status(404).json({
+      success: false,
+      message: e,
+    });
+  }
+});
+
+events.put("/:user_id/:event_id", async (req, res) => {
+  const { user_id, event_id } = req.params;
+
+  try {
+    const updated = await updateEvent(req.body, user_id, event_id);
+
+    if (updated.user_id) {
+      res.status(200).json({
+        success: true,
+        payload: updated,
+      });
+    } else {
+      throw updated;
+    }
+  } catch (e) {
+    res.status(404).json({
+      success: false,
+      message: e,
+    });
+  }
+});
+
+module.exports = events;
--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -4,7 +4,10 @@ INSERT INTO users ( username, password, email, created_on, last_login) VALUES
 ('tearle92' , 'fakepassword' ,'fakeemail@email.com','2004-10-19 10:23:54', '2004-10-19 10:23:54');
 
 INSERT INTO events ( event_name , event_zip, event_budget, event_date, event_time, user_id ) VALUES
-('lindas baby shower' , '11375', 1000, '2021-09-30', '00:30:00', 1);
+('Lindas Baby Shower' , '11375', 1000, '2021-09-30', '00:30:00', 1),
+('Saras Birthday Party' , '10025', 1500, '2021-09-30', '00:30:00', 1),
+('Michaels Retirement Party' , '10036', 2000, '2021-09-30', '00:30:00', 1)
+;
 
 INSERT INTO tasklist (task_name , is_completed , user_id, event_id ) VALUES 
 ('catering', false, 1, 1) ,
