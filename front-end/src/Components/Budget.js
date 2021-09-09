b4e930ca822ef6d47ae5f7d7646c1c899735c262
Teyanna Earle <teyonna.earle92@gmail.com>
2021-09-09 19:57:49 -0400

    Fix budget updates on event page. Change data type for task_cost in schema

--- front-end/src/Components/Budget.js
+++ front-end/src/Components/Budget.js
@@ -1,51 +1,20 @@
 import React, { useEffect, useState } from "react";
 
-function Budget({ categories, budget }) {
-  const [showForm, setShowForm] = useState({});
-  // const [costs, setCosts] = useState({});
-  const [shownCost, setShownCost] = useState({})
+function Budget({ categories, budget , shownCost}) {
   const [budgetStatus, setBudgetStatus] = useState(0)
   const formatter = new Intl.NumberFormat("en-US" , {
     style: "currency",
     currency: "USD"
   });
 
-  useEffect(() => {
-    let categoryStates = {};
-    let categoryCosts = {};
-    for (let category of categories) {
-      categoryStates[category.name] = false;
-      categoryCosts[category.name] = category.cost;
-    }
-
-    setShowForm(categoryStates);
-    // setCosts(categoryCosts);
-    setShownCost(categoryCosts)
-  }, [categories]);
-
-
 
   useEffect(() => {
     const values = Object.values(shownCost)
     const sum = values.reduce((a,b) =>{ return Number(a) + Number(b) }, 0)
     const currentSpending = Number(budget) - Number(sum)
-
     setBudgetStatus(currentSpending)
   }, [shownCost, budget])
 
-
-
-  // const handleFormChange = (e) => {
-  //   setCosts({ ...costs, [e.target.id]: e.target.value });
-  // };
-
-
-  // const handleFormSubmit = (e, category) => {
-  //   e.preventDefault();
-  //   // setShownCost(costs)
-  //   setShowForm({ ...showForm, [category]: false })
-
-  // }
 
 
   const listItem = (category) => {
@@ -85,24 +54,6 @@ function Budget({ categories, budget }) {
     return item;
   };
 
-  // const form = (category) => {
-  //   return (
-  //     <form onSubmit={(e) => handleFormSubmit(e, category)}>
-  //       <input
-  //         id={category}
-  //         placeholder="cost"
-  //         value={costs[category]}
-  //         onChange={handleFormChange}
-  //         type="number"
-  //         min="0"
-  //         step=".01"
-  //       />
-  //       <input type="submit" value="Update"/>
-  //     </form>
-  //   );
-  // };
-
-
   const budgetUpdate = () => {
 
     let status = ""
@@ -121,7 +72,6 @@ function Budget({ categories, budget }) {
   }
 
 
-
   return (
     <div>
        <p> Projected Budget: {formatter.format(budget)}</p>
@@ -132,14 +82,6 @@ function Budget({ categories, budget }) {
             <li key={i} className="budget-item">
               <p>{listItem(category.name)} </p>
               <p> { formatter.format(shownCost[category.name]) }</p>
-              {/* <button
-                onClick={() =>
-                  setShowForm({ ...showForm, [category.name]: !showForm[category.name] })
-                }
-              >
-                Edit Cost
-              </button> */}
-              {/* {showForm[category.name] ? form(category.name) : null} */}
             </li>
           );
         })}
