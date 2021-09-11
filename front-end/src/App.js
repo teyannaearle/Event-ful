5c748777a5ceab9ddbd0f9328f88fd4dac192279
Angelina Ebreo <angelinaebreo@gmail.com>
2021-09-11 16:32:42 -0400

    Auth.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import api from "./util/apiCalls";
 import Booked from "./Pages/Booked.js";
 import Dashboard from "./Pages/Dashboard.js";
@@ -9,47 +10,57 @@ import Landing from "./Pages/Landing.js";
 import SignUp from "./Pages/SignUp.js";
 import VendorIndex from "./Pages/VendorIndex.js";
 import VendorShow from "./Pages/VendorShow.js";
-import { Route, Switch } from "react-router-dom";
-import "./App.css";
+import Login from "./Components/Login";
+import { AuthProvider } from "./Components/Auth";
+import PrivateRoute from "./Components/PrivateRoute";
 import Test from "./Pages/Test";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <Switch>
-        <Route exact path="/">
-          <Landing />
-        </Route>
+      {/* <AuthProvider>
+        <Router> */}
+          <Switch>
+            <Route exact path="/">
+              <Landing />
+            </Route>
 
-        <Route path="/signup">
-          <SignUp />
-        </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
 
-        <Route path="/dashboard/:user_id">
-          <Dashboard />
-        </Route>
+            <Route path="/signup">
+              <SignUp />
+            </Route>
 
-        <Route path="/event/:user_id/:event_id">
-          <Event />
-        </Route>
+            <Route path="/dashboard/:user_id">
+              <Dashboard />
+            </Route>
 
-        <Route path="/vendor/:category/:provider_id">
-          <VendorShow />
-        </Route>
+            <Route path="/event/:user_id/:event_id">
+              <Event />
+            </Route>
 
-        <Route path="/favorites/:user_id">
-          <Favorites />
-        </Route>
+            <Route path="/vendor/:category/:provider_id">
+              <VendorShow />
+            </Route>
 
-        <Route path="/vendors/:category">
-          <VendorIndex />
-          {/* <Test /> */}
-        </Route>
+            <Route path="/favorites/:user_id">
+              <Favorites />
+            </Route>
 
-        <Route path="/booked/:user_id/:event_id">
-          <Booked />
-        </Route>
-      </Switch>
+            <Route path="/vendors/:category">
+              <VendorIndex />
+              {/* <Test /> */}
+            </Route>
+
+            <Route path="/booked/:user_id/:event_id">
+              <Booked />
+            </Route>
+          </Switch>
+        {/* </Router>
+      </AuthProvider> */}
     </div>
   );
 }
