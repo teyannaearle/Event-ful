b916b4c8a62e1f120c834806d95745eb75e9307d
cassidybeni <cassidybeni@pursuit.org>
2021-09-27 13:44:16 -0400

    Delete works oif page is refreshed

--- front-end/src/Components/Dashboard/Event.js
+++ front-end/src/Components/Dashboard/Event.js
@@ -1,20 +1,54 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import CapitalizeEvent from "../CapitalizeEvent";
+import axios from "axios";
+import { apiURL } from "../../util/apiURL";
+import { useState, useEffect } from "react";
 
-function Event({ event, user_id }) {
+const API = apiURL();
+
+function Event({ history, event, user_id }) {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${API}/events/${user_id}/${event.event_id}`)
+      .then(
+        (res) => {
+          console.log(res.data);
+          setEvents(res.data.payload.event_id);
+        },
+        (error) => {
+          history.push("/not-found");
+        }
+      )
+      .catch((c) => console.warn("catch", c));
+  }, [history, event.event_id]);
+
+  const handleDelete = () => {
+    axios
+      .delete(`${API}/events/${user_id}/${event.event_id}`)
+      .then(
+        () => history.push("/dashboard"),
+        (error) => console.warn(error)
+      )
+      .catch((c) => console.error(c));
+  };
+
   return (
     <div className="event-sq">
-
       <Link to={`/event/${event.event_id}`}>
         <p className="plan-ev">
           &#x261E;Plan {CapitalizeEvent(event.event_name)}
         </p>
       </Link>
       <span className="ed-but">
-      <Link to={`/dashboard/${user_id}/edit`}>
-        <button className="pg-buttons edit-ev">Edit Event Details</button>
-      </Link>
+        <Link to={`/dashboard/${user_id}/edit`}>
+          <button className="pg-buttons edit-ev">Edit Event Details</button>
+        </Link>
+        <button onClick={handleDelete} className="pg-buttons edit-ev">
+          Delete Event
+        </button>
       </span>
     </div>
   );
