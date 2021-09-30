8b305b309e88cbb42e08d6b464524964441b6252
Raymond Udeogu <rfizzle25@gmail.com>
2021-09-30 13:09:24 -0400

    event form page css

--- front-end/src/Pages/NewEventForm.js
+++ front-end/src/Pages/NewEventForm.js
@@ -1,10 +1,13 @@
 import React from "react";
 import EventForm from "../Components/EventForm";
 
-export default function NewEventForm({user_id}) {
+export default function NewEventForm({ user_id }) {
   return (
-    <div>
-      <EventForm user_id={user_id}/>
+    <div className="page">
+      <h1 className="pg-head">Create a New Event </h1>
+      <div className="dash-container three-d">
+        <EventForm user_id={user_id} />
+      </div>
     </div>
   );
 }
