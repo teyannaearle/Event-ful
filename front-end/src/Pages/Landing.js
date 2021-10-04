fc8e11116940e5a0ed3f51124c9ee7b3eb7a6dc8
Rayvon Finney Pinkston <rayfinn55@gmail.com>
2021-10-04 13:30:18 -0400

    landing image applied

--- front-end/src/Pages/Landing.js
+++ front-end/src/Pages/Landing.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Landing.css";
 import { Link } from "react-router-dom";
 import call2action from "../assets/call2action.jpg"
-import giphy from "../assets/giphy.gif"
+import LandingPageImage3 from "../assets/LandingPageImage3.png"
 
 export default function Landing() {
   return (
@@ -11,7 +11,8 @@ export default function Landing() {
         <section className="Landing-Item">
           <h1>EVENT( FUL ) &#127881;</h1>
           <img
-            src="https://s3-alpha.figma.com/hub/file/948140848/1f4d8ea7-e9d9-48b7-b70c-819482fb10fb-cover.png"
+          src={LandingPageImage3}
+            // src="https://s3-alpha.figma.com/hub/file/948140848/1f4d8ea7-e9d9-48b7-b70c-819482fb10fb-cover.png"
             alt="placeholder"
           />
           <br/>
