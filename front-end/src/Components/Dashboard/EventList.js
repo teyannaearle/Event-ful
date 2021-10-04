c38a22b571e2fcb93870f92e393da20efbda585c
Angelina Ebreo <angelinaebreo@gmail.com>
2021-10-04 14:14:25 -0400

    merging conflict

--- front-end/src/Components/Dashboard/EventList.js
+++ front-end/src/Components/Dashboard/EventList.js
@@ -7,17 +7,9 @@ import { UserContext } from "../../Providers/UserProvider.js";
 
 const API = apiURL();
 
-<<<<<<< HEAD
-function EventList() {
-  const loggedInUser = useContext(UserContext);
-  const [events, setEvents] = useState([]);
-  const user_id = loggedInUser ? loggedInUser.user_id : null;
-  console.log(`eventslist user_id is ${user_id}`);
-=======
 function EventList({ user_id }) {
   const [events, setEvents] = useState([]);
   const location = useLocation()
->>>>>>> b05922ff1baaeb34229f8b0b9cc80cb88997ac9d
 
   useEffect(() => {
     axios
@@ -33,14 +25,7 @@ function EventList({ user_id }) {
       .catch((e) => {
         console.error(e);
       });
-<<<<<<< HEAD
-      return () => {
-     setEvents([])
-      }
-  }, [user_id]);
-=======
   }, [user_id, location.pathname]);
->>>>>>> b05922ff1baaeb34229f8b0b9cc80cb88997ac9d
 
   // console.log(location)
   const handleDelete = async (event_id) => {
