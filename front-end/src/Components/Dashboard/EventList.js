356ae64bdfa70da24a8cf8b14b07532d55720c19
Teyanna Earle <teyonna.earle92@gmail.com>
2021-10-05 12:33:29 -0400

    css

--- front-end/src/Components/Dashboard/EventList.js
+++ front-end/src/Components/Dashboard/EventList.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 function EventList({ user_id, events, deleteEvent }) {
   return (
     <>
-      <ul className="dash-events">
+      <ul className={`dash-events ${events.length <=3 ? "inital-box" : null}` }>
         <span className="dash-event new-sq">
           <Link to={`/dashboard/new_event`} className="new-event">
             <p id="new">Create a new event and start planning! </p>
