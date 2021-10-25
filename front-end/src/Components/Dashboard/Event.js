62c567ee5258d61a4e3b4c689a0d5b7ab006715e
Teyanna Earle <teyonna.earle92@gmail.com>
2021-10-25 12:27:14 -0400

    fix emojis on dash

--- front-end/src/Components/Dashboard/Event.js
+++ front-end/src/Components/Dashboard/Event.js
@@ -8,21 +8,25 @@ function Event({ event, deleteEvent }) {
     const name = event.event_name.toLowerCase();
     if (name.includes("baby" || "babies")) {
       item = <>&#128118;</>;
-    } else if (name.includes("birthday" || "surprise")) {
+    } else if (name.includes("birthday") || name.includes("surprise")) {
       item = <>&#129395;</>;
     } else if (name.includes("retire")) {
       item = <>&#127870;</>;
     } else if (name.includes("new")) {
       item = <>&#127879;</>;
-    } else if (name.includes("holiday" || "christmas")) {
-      item = <>&#65039;</>;
+    } else if (name.includes("holiday") || name.includes( "christmas")) {
+      item = <>&#129507;</>;
     } else if (name.includes("halloween")) {
       item = <>&#127875;</>;
-    } else if (name.includes("sweet" || "quince")){
+    } else if (name.includes("sweet") || name.includes("quince")){
       item = <>&#128131;</> ;
     } else if (name.includes("brid")){
       item = <>&#x1F470;</> 
-    }else {
+    } else if (name.includes("grad") || name.includes("commencement")){
+      console.log("yes")
+      item = <>&#127891;</>
+    } else {
+      console.log("no")
       item = <>&#127882;</>;
     }
 
