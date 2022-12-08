9a53f0a46fe46b113c4e5957b2e7770970133ed0
Teyanna Earle <teyonna.earle92@gmail.com>
2022-12-07 21:28:59 -0500

    temp change to heroku

--- front-end/src/util/apiURL.js
+++ front-end/src/util/apiURL.js
@@ -1,6 +1,7 @@
 export const apiURL = () => {
     return window.location.hostname === "localhost"
     ?"http://localhost:3003"
-      : "https://eventful.adaptable.app";
+      : "https://eventful-application.herokuapp.com"
+      // "https://eventful.adaptable.app";
   };
   
