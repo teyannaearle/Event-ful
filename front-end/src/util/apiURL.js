565f4de1e75d37876c86762366472034238214ce
Teyanna Earle <teyonna.earle92@gmail.com>
2022-12-07 19:31:49 -0500

    change apiURL

--- front-end/src/util/apiURL.js
+++ front-end/src/util/apiURL.js
@@ -1,7 +1,8 @@
 export const apiURL = () => {
-    return window.location.hostname === "localhost"
-    ? "https://eventful-application-db.herokuapp.com"
-      // ? "http://localhost:3333"
-      : "https://eventful-application-db.herokuapp.com";
+    return "https://eventful.adaptable.app"
+    // window.location.hostname === "localhost"
+    // ? "https://eventful-application-db.herokuapp.com"
+    //   // ? "http://localhost:3333"
+    //   : "https://eventful-application-db.herokuapp.com";
   };
   
